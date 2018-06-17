49ea9d21027b482728d257a228f20f79dd1c899b
yarplux <yarplux@yandex.ru>
2018-06-17 13:35:12 +0300

    Задание 1. Версия "обычная". Функционал:  - Список.  - По нажатию на элемент списка - открытие фрагмента с кнопкой (стрелка назад).  - Переход назад по нажатию на стрелку

--- app/src/main/java/com/shifu/user/project1/MainActivity.java
+++ app/src/main/java/com/shifu/user/project1/MainActivity.java
@@ -1,14 +1,27 @@
 package com.shifu.user.project1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // экземпляр FragmentTransaction
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+
+        // фрагмент
+        list frag1 = new list();
+        fragmentTransaction.add(R.id.container, frag1);
+        fragmentTransaction.commit();
+
     }
 }
--- app/src/main/java/com/shifu/user/project1/list.java
+++ app/src/main/java/com/shifu/user/project1/list.java
@@ -1,29 +1,41 @@
 package com.shifu.user.project1;
 
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.app.FragmentTransaction;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class list extends Fragment {
 
+public class list extends ListFragment {
 
-    public list() {
-        // Required empty public constructor
-    }
-
+    NewFragment frag2;
+    FragmentTransaction fTrans;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String[] animals = getResources().getStringArray(R.array.animals);
+        ListAdapter adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, animals);
+        setListAdapter(adapter);
+
+        frag2 = new NewFragment();
+
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        fTrans = getFragmentManager().beginTransaction();
+        fTrans.replace(R.id.container, frag2);
+        fTrans.addToBackStack(null);
+        fTrans.commit();
+    }
+
 
 }
